3bf31956ff0bf6e68439ce638a8654b5ca2ad4e3
Henry <hzhang5@stuy.edu>
2015-11-17 21:23:18 -0500

    First commit

---
+++ Rational.java
@@ -0,0 +1,11 @@
+public class Rational{
+    private int numerator;
+    private int denominator;
+    public Rational(){
+	numerator=0;
+	denominator=1;
+    }
+    public Rational(int num, int den){
+	public 
+	    }
+}
